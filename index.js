43b05e3ef8e6898614b45a40b46e81804786c92d
i-zokirov <izokirov@outlook.com>
2022-11-19 12:49:47 +0100

    added a route

--- index.js
+++ index.js
@@ -27,37 +27,38 @@ function toTimestamp(strDate) {
     return Date.parse(strDate);
 }
 
+app.get("/api", function (req, res) {
+    const now = new Date();
+    res.json({
+        unix: now.valueOf(),
+        utc: now.toUTCString(),
+    });
+});
+
 app.get("/api/:date", function (req, res) {
     let dateString = req.params.date;
 
     //A 4 digit number is a valid ISO-8601 for the beginning of that year
     //5 digits or more must be a unix time, until we reach a year 10,000 problem
-    if (dateString) {
-        if (/\d{5,}/.test(dateString)) {
-            let dateInt = parseInt(dateString);
-            //Date regards numbers as unix timestamps, strings are processed differently
-            res.json({
-                unix: dateString,
-                utc: new Date(dateInt).toUTCString(),
-            });
-        } else {
-            let dateObject = new Date(dateString);
 
-            if (dateObject.toString() === "Invalid Date") {
-                res.json({ error: "Invalid Date" });
-            } else {
-                res.json({
-                    unix: dateObject.valueOf(),
-                    utc: dateObject.toUTCString(),
-                });
-            }
-        }
-    } else {
-        const now = new Date();
+    if (/\d{5,}/.test(dateString)) {
+        let dateInt = parseInt(dateString);
+        //Date regards numbers as unix timestamps, strings are processed differently
         res.json({
-            unix: now.valueOf(),
-            utc: now.toUTCString(),
+            unix: dateString,
+            utc: new Date(dateInt).toUTCString(),
         });
+    } else {
+        let dateObject = new Date(dateString);
+
+        if (dateObject.toString() === "Invalid Date") {
+            res.json({ error: "Invalid Date" });
+        } else {
+            res.json({
+                unix: dateObject.valueOf(),
+                utc: dateObject.toUTCString(),
+            });
+        }
     }
 });
 
